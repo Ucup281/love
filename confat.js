78da20ae1ff4395581e3f54eb4899b91666334e1
Ucup280 <ucupkun130@gmail.com>
2022-02-04 09:11:14 +0700

    love

---
+++ confat.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+const chalk = require('chalk')
+const setting = JSON.parse(fs.readFileSync('./setting.json'));
+
+// Other
+global.owner = ["6281220582411","6281220582411","6281220582411"]
+global.packname = '©「 Ｕｃｕｐ－Ｋｕｎ  Ｂｏｔｚ 」'
+global.youtube = 'https://youtube.com/'
+global.author = '「  Ｕｃｕｐ－Ｋｕｎ  Ｂｏｔｚ🦅  」'
+global.sessionName = 'session'
+global.flaming = "https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text="
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    sukses: '🤗Done, Oke Desu~',
+    admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
+    botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
+    owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
+    group: 'Perintah Ini Hanya Bisa Digunakan Di Group !',
+    private: 'Perintah Ini Hanya Bisa Digunakan Di Private !',
+	bot: 'Fitur Khusus Pengguna Nomor Bot',
+    errtoimg: 'Maaf Saat Ini Belum Support Sticker Gif !',
+    wait: '⏳ Sedang Di Proses',
+	lockCmd: 'Fitur Tidak Diaktifkan Oleh Owner!',
+	example1: 'Selamat Datang Beban @user Di Group @subject Jangan Lupa Baca Rules Ya Sayang @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name)'
+}
+global.thumb = fs.readFileSync(setting.thumbnail)
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
